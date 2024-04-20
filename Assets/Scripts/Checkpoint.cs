d63a2f35fbba2535ebe2c68090563bc9ab30ab24
usc-CindyChow123 <47686893+CindyChow123@users.noreply.github.com>
2024-04-20 16:02:02 -0700

    add text for checkpoints

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -8,6 +8,7 @@ public class Checkpoint : MonoBehaviour
 
     GameManager gameManager;
     Collider2D coll;
+    public GameObject checkpointText;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -18,7 +19,7 @@ public class Checkpoint : MonoBehaviour
     private void Start()
     {
         circleRenderer = this.transform.GetChild(0).GetComponent<SpriteRenderer>();
-
+        checkpointText = gameManager.ckptText;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,6 +30,19 @@ public class Checkpoint : MonoBehaviour
             gameManager.UpdateCheckpointPosition(transform.position);
             circleRenderer.color = Color.white;
             coll.enabled = false;
+            showText();
         }
+    }
+    
+    private void showText()
+    {
+        StartCoroutine(ShowUIText());
+    }
+    
+    private IEnumerator ShowUIText()
+    {
+        checkpointText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        checkpointText.gameObject.SetActive(false);
     }
 }
